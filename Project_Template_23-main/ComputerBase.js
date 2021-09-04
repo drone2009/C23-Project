7cee17f02eed9a7a3f40d6164dbe8fa7c550b68c
drone2009 <85007509+drone2009@users.noreply.github.com>
2021-09-04 09:17:50 +0530

    Add files via upload

---
+++ Project_Template_23-main/ComputerBase.js
@@ -0,0 +1,26 @@
+class computerbase {
+    constructor (x, y, width, height) {
+        var options = {
+            isstatic: true 
+        };
+        this.body = Bodies.rectangle(x, y, width, height, options); 
+        this.width = width;
+        this.height = height; 
+        this.image = loadimage("/.assets/base2.png");
+
+        world.add (world, this.body);
+
+    }
+
+    display() {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push(); 
+        translate(pos.x, pos.y); 
+        rotate(angle); 
+        imagemode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+
+        pop();
+    }
+}
